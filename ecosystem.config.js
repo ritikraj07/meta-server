5f7791db9171eeb9c9a65cdc98cab77e8a58ace8
Ritik Raj <117502397+ritikraj07@users.noreply.github.com>
2024-07-29 21:06:10 +0530

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "meta-server",
+      script: "npm",
+      args: "start",
+      watch: true,
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
