b36e5afcb6b5231bca61ee11cbbe790e6008074a
ritikraj07 <ritikra3rrr@gmail.com>
2024-08-05 11:38:25 +0530

    package json change

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,9 @@ import cors from "cors";
 const { __dirname } = getDirnameAndFilename(import.meta.url);
 
 const app = express();
-const PORT = 3000;
+// Use environment variable PORT or default to 3000
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public/dist")));
